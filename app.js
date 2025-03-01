02ecf63977cbfe072eb9a16ecb96da59ff9db28c
Denis <d_sudnikov@mail.ru>
2025-03-01 21:37:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//to compiling use tsc app.ts or tsc app.ts --watch
+console.log("Hello World!");
+// @ts-ignore
+console.log(2 + "2" - 2);
+console.log("Third commit");
+var isValid = function (number, base) {
+    if (base === void 0) { base = ''; }
+    if (base == "base") {
+        return (typeof (+number) == "number" && +number % 1 === 0 && number >= 2 && number <= 36);
+    }
+    else {
+        return (typeof (+number) == "number" && +number % 1 === 0);
+    }
+};
+function myPrompt() {
+    var myNumber;
+    var base;
+    while (true) {
+        myNumber = prompt("Enter your number");
+        if (myNumber == null)
+            return alert("bye-bye");
+        if (!isValid(myNumber)) {
+            alert("Please enter a valid number");
+        }
+        else {
+            break;
+        }
+    }
+    while (true) {
+        base = prompt("Enter your number");
+        if (base == null)
+            return alert("bye-bye");
+        if (!isValid(base, "base")) {
+            alert("Please enter a valid base");
+        }
+        else {
+            break;
+        }
+    }
+    var result = parseInt(myNumber).toString(Number(base));
+    alert("The number ".concat(myNumber, " in base ").concat(base, " is ").concat(result, "."));
+}
+myPrompt();
--- app.ts
+++ app.ts
@@ -1,5 +1,43 @@
+//to compiling use tsc app.ts or tsc app.ts --watch
+
 console.log("Hello World!");
 // @ts-ignore
 console.log(2 + "2" - 2);
 
 console.log("Third commit");
+
+const isValid = (number: any, base: "" | "base" = ''): boolean => {
+    if (base == "base") {
+        return (typeof (+number) == "number" && +number % 1 === 0 && number >= 2 && number <= 36)
+    } else {
+        return (typeof (+number) == "number" && +number % 1 === 0)
+    }
+}
+
+function myPrompt() {
+    let myNumber: any;
+    let base: any;
+    while (true) {
+        myNumber = prompt("Enter your number");
+        if (myNumber == null) return alert("bye-bye");
+        if (!isValid(myNumber)) {
+            alert("Please enter a valid number");
+        } else {
+            break;
+        }
+    }
+    while (true) {
+        base = prompt("Enter your number");
+        if (base == null) return alert("bye-bye");
+        if (!isValid(base, "base")) {
+            alert("Please enter a valid base");
+        } else {
+            break;
+        }
+    }
+    const result = parseInt(myNumber).toString(Number(base));
+    alert(`The number ${myNumber} in base ${base} is ${result}.`);
+}
+
+myPrompt()
+
