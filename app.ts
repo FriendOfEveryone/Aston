567b1e4315eea0fb358d6f0b80e59dfc929defc2
Denis <d_sudnikov@mail.ru>
2025-03-09 17:41:45 +0300

    first commit

--- app.ts
+++ app.ts
@@ -1,5 +1,39 @@
-console.log("Hello World!");
-// @ts-ignore
-console.log(2 + "2" - 2);
+const originalObj = {
+    a: 3,
+    b: {
+        c: 7
+    }
+}
+let newObj = {}
 
-console.log("Third commit");
+const deepCopyOne = (obj: Object) => {
+    return JSON.parse(JSON.stringify(obj));
+}
+
+function deepCopyTwo(obj: any) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    const copy = Array.isArray(obj) ? [] : {};
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            copy[key] = deepCopyTwo(obj[key]);
+        }
+    }
+    return copy;
+}
+
+newObj = deepCopyOne(originalObj);
+
+console.log(newObj);
+
+newObj = structuredClone(originalObj);
+
+console.log(newObj);
+
+newObj = deepCopyTwo(originalObj);
+
+console.log(newObj);
+
+
+
