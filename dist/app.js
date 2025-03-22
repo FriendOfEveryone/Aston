70cc751b36e22186b69b6f19c432e428c25d74ac
Denis <d_sudnikov@mail.ru>
2025-03-22 13:47:15 +0300

    hw7

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,112 @@
 "use strict";
 // console.log("Hello World!")
+class Vehicle {
+    constructor(brand, model, year, _speed) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.speed = _speed;
+    }
+    accelerate(amount) {
+        this.speed = amount;
+    }
+    ;
+    brake(amount) {
+        this.speed -= amount;
+    }
+    ;
+    info() {
+        console.log(`
+        Бренд: ${this.brand} 
+        Модель: ${this.model} 
+        Год: ${this.year} 
+        Текущая/Максимальная скорость: ${this.speed}
+        `);
+    }
+    ;
+    drive() {
+        console.log(`Поехали ${this.model}!`);
+    }
+    static comparison(car1, car2) {
+        if (car1.year <= car2.year)
+            return -1;
+        else
+            return 1;
+    }
+}
+class Car extends Vehicle {
+    constructor(brand, model, year, speed, fuelType, fuelLevel) {
+        super(brand, model, year, speed);
+        this.fuelType = '';
+        this.fuelLevel = 0;
+        if (fuelType)
+            this.fuelType = fuelType;
+        if (fuelLevel)
+            this.fuelLevel = fuelLevel;
+    }
+    refuel() {
+        return `${this.fuelType} ${this.fuelLevel}`;
+    }
+    info() {
+        console.log(`
+        Бренд: ${this.brand} 
+        Модель: ${this.model} 
+        Год: ${this.year} 
+        Текущая/Максимальная скорость: ${this.speed}
+        Заправена на: ${this.fuelLevel}
+        Тип топлива: ${this.fuelType}
+        `);
+    }
+}
+class ElectricCar extends Car {
+    constructor(brand, model, year, speed, batteryLevel, fuelType) {
+        super(brand, model, year, speed, fuelType);
+        this._batteryLevel = batteryLevel;
+    }
+    get batteryLevel() {
+        return this._batteryLevel;
+    }
+    set batteryLevel(value) {
+        this._batteryLevel = value;
+    }
+    charge(value) {
+        if (value < 0)
+            return;
+        if (this._batteryLevel + value >= 100) {
+            this._batteryLevel = 100;
+            return;
+        }
+        else {
+            this._batteryLevel += value;
+        }
+    }
+    ;
+    refuel() {
+        return `${this.batteryLevel}`;
+    }
+    info() {
+        console.log(`
+        Бренд: ${this.brand} 
+        Модель: ${this.model} 
+        Год: ${this.year} 
+        Текущая/Максимальная скорость: ${this.speed}
+        Заряд батареи: ${this.batteryLevel}
+        Тип топлива: ${this.fuelType}
+        `);
+    }
+}
+const vehicle = new Vehicle('Simple', 'veh', 1999, 100);
+vehicle.info();
+const car = new Car('Viper', 'Dodge', 3000, 100, '99', 30);
+car.info();
+const electricCar = new ElectricCar('Tesla', 'CyberTrack', 5000, 250, 34, 'battery');
+electricCar.info();
+const testDrive = (car) => {
+    car.drive();
+};
+testDrive(vehicle);
+testDrive(car);
+testDrive(electricCar);
+// Методы Drivable уже описаны в Vehicle
+const carArr = [car, electricCar, vehicle];
+console.log(carArr.sort(Vehicle.comparison));
